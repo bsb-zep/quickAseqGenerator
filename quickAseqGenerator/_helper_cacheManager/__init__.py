5fff15f964bd550a92dbfb9f9640156fdf0f8d89
bkroll <16916-bkroll@users.noreply.gitlab.lrz.de>
2020-02-16 18:59:10 +0100

    add getFromCache function

--- quickAseqGenerator/_helper_cacheManager/__init__.py
+++ quickAseqGenerator/_helper_cacheManager/__init__.py
@@ -6,3 +6,12 @@ cacheDir = './bvCache/'
 def writeCacheEntry(bvId, cacheLine):
     with open(cacheDir + bvId, 'w') as cacheEntryDao:
         cacheEntryDao.write(cacheLine)
+
+def getFromCache(bvId):
+    try:
+        with open(cacheDir + bvId, 'r') as cacheEntryDao:
+            cacheContent = json.load(cacheEntryDao)
+        return cacheContent
+    except FileNotFoundError:
+        print('bvId not in cache')
+        return False
